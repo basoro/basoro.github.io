d11746018f8c100f1f9e37fee32ea0f3914d71b0
Basoro <dentix.id@gmail.com>
2018-11-19 10:09:25 +0800

    Update simrs-khanza.js

--- assets/js/simrs-khanza.js
+++ assets/js/simrs-khanza.js
@@ -33,7 +33,7 @@ function GetReleases(repo) {
                             .attr("href", release.html_url)
                             .text(release.name)
                         )
-                        .append(release.prerelease ? "<div style=\"float:right;\"><span style=\"color:red;\">Pre-release</span></div>" : "")
+                        .append(release.prerelease ? "<div style=\"display:inline;\"><span style=\"background-color:red;color:white;\">Pre-release</span></div>" : "")
                     )
                     .append($("<td>")
                         .append($("<a>")
