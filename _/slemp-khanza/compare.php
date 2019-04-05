a2a6a56794a2985def71ed76088b985cac446680
tiredbug <drg.faisol@basoro.org>
2019-04-05 07:00:01 +0700

    update

--- _/slemp-khanza/compare.php
+++ _/slemp-khanza/compare.php
@@ -26,48 +26,19 @@ $dbs_config = array(
 include_once './public/head.html';
 include_once './public/menu.html';
 
-function show_options($dbs_config)
-{
-    echo '<p class="info">Once two database schemas have been exported, paste them here to be compared.</p>';
-
-    echo '<form action="?a=compare" method="post" id="compare">';
-    if (count($dbs_config) < 2) {
-        echo '<div class="field"><label for="schema1">First schema</label><textarea name="schema1" id="schema1" cols="100" rows="5"></textarea></div>';
-        echo '<div class="field"><label for="schema2">Second schema</label><textarea name="schema2" id="schema2" cols="100" rows="5"></textarea></div>';
-    } else {
-        echo '<div class=""><label for="schema1">First schema</label><select name="schema1">';
-        foreach ($dbs_config as $key => $db_config) {
-            echo '<option value=' . $key . '>' . $db_config['name'] . '</option>';
-        }
-        echo '</select></div>';
-        echo '<div class=""><label for="schema2">Second schema</label><select name="schema2">';
-        foreach ($dbs_config as $key => $db_config) {
-            echo '<option value=' . $key . '>' . $db_config['name'] . '</option>';
-        }
-        echo '</select></div>';
-    }
-
-    echo '<div class="submit"><input type="submit" value="Compare" /></div>';
-    echo '</form>';
-}
-
-function echo_error($error)
-{
+function echo_error($error){
     echo '<p class="error">', $error, '</p>';
 }
 
-function strip_nl($str)
-{
+function strip_nl($str){
     return str_replace(array("\n", "\r"), '', $str);
 }
 
-function s($count)
-{
+function s($count){
     return $count != 1 ? 's' : '';
 }
 
-function do_compare($schema1, $schema2)
-{
+function do_compare($schema1, $schema2){
     if (empty($schema1) || empty($schema2)) {
         echo_error('Both schemas must be given.');
         return;
@@ -80,7 +51,7 @@ function do_compare($schema1, $schema2)
 
     if (count($results) > 0) {
 
-        echo '<h3>Found differences in ' . count($results) . ' table' . s(count($results)) . ':</h3>';
+        echo '<div class="important-title"><span class="glyphicon glyphicon-alert" style="color: #f39c12; margin-right: 10px;"></span>Found differences in ' . count($results) . ' table' . s(count($results)) . ':</div>';
 
         echo '<ul id="differences">';
         foreach ($results as $table_name => $differences) {
@@ -96,54 +67,63 @@ function do_compare($schema1, $schema2)
         echo '<p>No differences found.</p>';
     }
 }
+
+function show_options($dbs_config){
+    echo '<form action="?a=compare" method="post" id="compare">';
+        echo '<p><span class="set-tit">First schema</span><select name="schema1">';
+        foreach ($dbs_config as $key => $db_config) {
+            echo '<option value=' . $key . '>' . $db_config['name'] . '</option>';
+        }
+        echo '</select></p>';
+        echo '<p><span class="set-tit">Second schema</span><select name="schema2">';
+        foreach ($dbs_config as $key => $db_config) {
+            echo '<option value=' . $key . '>' . $db_config['name'] . '</option>';
+        }
+        echo '</select></p>';
+
+    echo '<div style="margin: 20px 0 10px 100px;"><input type="submit" class="btn btn-info btn-sm btn-title" value="Compare" /></div>';
+    echo '</form>';
+}
+
 ?>
 
-<div class="main-content">
+										<div class="main-content">
                         <div class="container-fluid">
-                                <div class="pos-ser">
-                                        <div class="position">
-                                                <a href="/index.php">Home</a>/<span>Database Compare</span>
-                                        </div>
+                            <div class="pos-ser">
+                                <div class="position">
+                                    <a href="/index.php">Home</a>/<span>Database Compare</span>
                                 </div>
-                        </div>
+                            </div>
 
-        <div id="canvas">
-            <h1><a href="?">DbDiff</a></h1>
-            <h2>Tool for comparing database schemas.</h2>
+        										<div class="setbox">
+				<div class="s-title">
+					<h3>Comparing database schemas</h3>
+				</div>
+				<div class="setting-con">
 
-            <?php
-            $action = @$_GET['a'];
+            										<?php
+            										$action = @$_GET['a'];
+            										switch ($action) {
+                										case 'compare':
 
-            switch ($action) {
+                    										$schema1 = $dbs_config[@$_POST['schema1']];
+                    										$schema2 = $dbs_config[@$_POST['schema2']];
 
-                case 'compare':
-                    if (count($dbs_config) < 2) {
-                        $schema1 = @$_POST['schema1'];
-                        $schema2 = @$_POST['schema2'];
+                    										$schema1 = serialize(DbDiff::export($schema1['config'], $schema1['name']));
+                    										$schema2 = serialize(DbDiff::export($schema2['config'], $schema2['name']));
 
-                        if (get_magic_quotes_gpc()) { // sigh...
-                            $schema1 = stripslashes($schema1);
-                            $schema2 = stripslashes($schema2);
-                        }
-                    } else {
-                        $schema1 = $dbs_config[@$_POST['schema1']];
-                        $schema2 = $dbs_config[@$_POST['schema2']];
+                    										do_compare($schema1, $schema2);
 
-                        $schema1 = serialize(DbDiff::export($schema1['config'], $schema1['name']));
-                        $schema2 = serialize(DbDiff::export($schema2['config'], $schema2['name']));
-                    }
+                    								break;
+                										default:
+                    										show_options($dbs_config);
+            										}
+            										?>
+        										</div>
+                                                  </div>
 
-                    do_compare($schema1, $schema2);
-
-                    echo '<p><a href="?">&laquo; Back to main page</a></p>';
-                    break;
-                default:
-                    show_options($dbs_config);
-            }
-            ?>
-        </div>
-
-</div>
+                      	</div>
+										</div>
 
 <?php
 //include_once './public/compare.html';
