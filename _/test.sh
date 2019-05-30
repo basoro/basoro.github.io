6fe4220ad66a13a6bb29fd886141bced831f619f
Basoro <dentix.id@gmail.com>
2019-05-31 04:53:57 +0800

    Update test.sh

--- _/test.sh
+++ _/test.sh
@@ -300,7 +300,7 @@ else
   chmod +x /etc/init.d/php-fpm-${php_version}
   /etc/init.d/php-fpm-${php_version} start
 fi
-echo $vphp > /www/server/php.pl
+echo $php_version > /www/server/php/default.pl
 
 #################
 # Install cloud #
