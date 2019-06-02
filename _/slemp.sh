0359ad9a030fa8de0fa6c0e176b97f5b423fac77
Basoro <dentix.id@gmail.com>
2019-06-02 14:51:23 +0800

    Update slemp.sh

--- _/slemp.sh
+++ _/slemp.sh
@@ -323,6 +323,8 @@ php_admin_flag[log_errors] = on
 php_admin_value[upload_max_filesize] = 32M
 END
 
+PHP_Opt
+
 rm -f /etc/init.d/nginx
 wget -O /etc/init.d/nginx basoro.id/downloads/nginx.init -T20
 chmod +x /etc/init.d/nginx
@@ -431,6 +433,8 @@ write_buffer = 2M
 [mysqlhotcopy]
 interactive-timeout
 EOF
+
+MySQL_Opt
 
 #################
 # Install Panel #
@@ -559,6 +563,104 @@ echo
 echo
 echo
 exit
+
+}
+
+PHP_Opt()
+{
+    if [[ ${MemTotal} -gt 1024 && ${MemTotal} -le 2048 ]]; then
+        sed -i "s#pm.max_children.*#pm.max_children = 30#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.start_servers.*#pm.start_servers = 5#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.min_spare_servers.*#pm.min_spare_servers = 5#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.max_spare_servers.*#pm.max_spare_servers = 30#" ${php_conf}/etc/php-fpm.conf
+    elif [[ ${MemTotal} -gt 2048 && ${MemTotal} -le 4096 ]]; then
+        sed -i "s#pm.max_children.*#pm.max_children = 50#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.start_servers.*#pm.start_servers = 10#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.min_spare_servers.*#pm.min_spare_servers = 10#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.max_spare_servers.*#pm.max_spare_servers = 50#" ${php_conf}/etc/php-fpm.conf
+    elif [[ ${MemTotal} -gt 4096 && ${MemTotal} -le 8192 ]]; then
+        sed -i "s#pm.max_children.*#pm.max_children = 80#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.start_servers.*#pm.start_servers = 15#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.min_spare_servers.*#pm.min_spare_servers = 15#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.max_spare_servers.*#pm.max_spare_servers = 80#" ${php_conf}/etc/php-fpm.conf
+    elif [[ ${MemTotal} -gt 8192 ]]; then
+        sed -i "s#pm.max_children.*#pm.max_children = 100#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.start_servers.*#pm.start_servers = 20#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.min_spare_servers.*#pm.min_spare_servers = 20#" ${php_conf}/etc/php-fpm.conf
+        sed -i "s#pm.max_spare_servers.*#pm.max_spare_servers = 100#" ${php_conf}/etc/php-fpm.conf
+    fi
+}
+
+
+MySQL_Opt()
+{
+    if [[ ${MemTotal} -gt 1024 && ${MemTotal} -lt 2048 ]]; then
+        sed -i "s#^key_buffer_size.*#key_buffer_size = 32M#" /etc/my.cnf
+        sed -i "s#^table_open_cache.*#table_open_cache = 128#" /etc/my.cnf
+        sed -i "s#^sort_buffer_size.*#sort_buffer_size = 768K#" /etc/my.cnf
+        sed -i "s#^read_buffer_size.*#read_buffer_size = 768K#" /etc/my.cnf
+        sed -i "s#^myisam_sort_buffer_size.*#myisam_sort_buffer_size = 8M#" /etc/my.cnf
+        sed -i "s#^thread_cache_size.*#thread_cache_size = 16#" /etc/my.cnf
+        sed -i "s#^query_cache_size.*#query_cache_size = 16M#" /etc/my.cnf
+        sed -i "s#^tmp_table_size.*#tmp_table_size = 32M#" /etc/my.cnf
+        sed -i "s#^innodb_buffer_pool_size.*#innodb_buffer_pool_size = 128M#" /etc/my.cnf
+        sed -i "s#^innodb_log_file_size.*#innodb_log_file_size = 32M#" /etc/my.cnf
+    elif [[ ${MemTotal} -ge 2048 && ${MemTotal} -lt 4096 ]]; then
+        sed -i "s#^key_buffer_size.*#key_buffer_size = 64M#" /etc/my.cnf
+        sed -i "s#^table_open_cache.*#table_open_cache = 256#" /etc/my.cnf
+        sed -i "s#^sort_buffer_size.*#sort_buffer_size = 1M#" /etc/my.cnf
+        sed -i "s#^read_buffer_size.*#read_buffer_size = 1M#" /etc/my.cnf
+        sed -i "s#^myisam_sort_buffer_size.*#myisam_sort_buffer_size = 16M#" /etc/my.cnf
+        sed -i "s#^thread_cache_size.*#thread_cache_size = 32#" /etc/my.cnf
+        sed -i "s#^query_cache_size.*#query_cache_size = 32M#" /etc/my.cnf
+        sed -i "s#^tmp_table_size.*#tmp_table_size = 64M#" /etc/my.cnf
+        sed -i "s#^innodb_buffer_pool_size.*#innodb_buffer_pool_size = 256M#" /etc/my.cnf
+        sed -i "s#^innodb_log_file_size.*#innodb_log_file_size = 64M#" /etc/my.cnf
+    elif [[ ${MemTotal} -ge 4096 && ${MemTotal} -lt 8192 ]]; then
+        sed -i "s#^key_buffer_size.*#key_buffer_size = 128M#" /etc/my.cnf
+        sed -i "s#^table_open_cache.*#table_open_cache = 512#" /etc/my.cnf
+        sed -i "s#^sort_buffer_size.*#sort_buffer_size = 2M#" /etc/my.cnf
+        sed -i "s#^read_buffer_size.*#read_buffer_size = 2M#" /etc/my.cnf
+        sed -i "s#^myisam_sort_buffer_size.*#myisam_sort_buffer_size = 32M#" /etc/my.cnf
+        sed -i "s#^thread_cache_size.*#thread_cache_size = 64#" /etc/my.cnf
+        sed -i "s#^query_cache_size.*#query_cache_size = 64M#" /etc/my.cnf
+        sed -i "s#^tmp_table_size.*#tmp_table_size = 64M#" /etc/my.cnf
+        sed -i "s#^innodb_buffer_pool_size.*#innodb_buffer_pool_size = 512M#" /etc/my.cnf
+        sed -i "s#^innodb_log_file_size.*#innodb_log_file_size = 128M#" /etc/my.cnf
+    elif [[ ${MemTotal} -ge 8192 && ${MemTotal} -lt 16384 ]]; then
+        sed -i "s#^key_buffer_size.*#key_buffer_size = 256M#" /etc/my.cnf
+        sed -i "s#^table_open_cache.*#table_open_cache = 1024#" /etc/my.cnf
+        sed -i "s#^sort_buffer_size.*#sort_buffer_size = 4M#" /etc/my.cnf
+        sed -i "s#^read_buffer_size.*#read_buffer_size = 4M#" /etc/my.cnf
+        sed -i "s#^myisam_sort_buffer_size.*#myisam_sort_buffer_size = 64M#" /etc/my.cnf
+        sed -i "s#^thread_cache_size.*#thread_cache_size = 128#" /etc/my.cnf
+        sed -i "s#^query_cache_size.*#query_cache_size = 128M#" /etc/my.cnf
+        sed -i "s#^tmp_table_size.*#tmp_table_size = 128M#" /etc/my.cnf
+        sed -i "s#^innodb_buffer_pool_size.*#innodb_buffer_pool_size = 1024M#" /etc/my.cnf
+        sed -i "s#^innodb_log_file_size.*#innodb_log_file_size = 256M#" /etc/my.cnf
+    elif [[ ${MemTotal} -ge 16384 && ${MemTotal} -lt 32768 ]]; then
+        sed -i "s#^key_buffer_size.*#key_buffer_size = 512M#" /etc/my.cnf
+        sed -i "s#^table_open_cache.*#table_open_cache = 2048#" /etc/my.cnf
+        sed -i "s#^sort_buffer_size.*#sort_buffer_size = 8M#" /etc/my.cnf
+        sed -i "s#^read_buffer_size.*#read_buffer_size = 8M#" /etc/my.cnf
+        sed -i "s#^myisam_sort_buffer_size.*#myisam_sort_buffer_size = 128M#" /etc/my.cnf
+        sed -i "s#^thread_cache_size.*#thread_cache_size = 256#" /etc/my.cnf
+        sed -i "s#^query_cache_size.*#query_cache_size = 256M#" /etc/my.cnf
+        sed -i "s#^tmp_table_size.*#tmp_table_size = 256M#" /etc/my.cnf
+        sed -i "s#^innodb_buffer_pool_size.*#innodb_buffer_pool_size = 2048M#" /etc/my.cnf
+        sed -i "s#^innodb_log_file_size.*#innodb_log_file_size = 512M#" /etc/my.cnf
+    elif [[ ${MemTotal} -ge 32768 ]]; then
+        sed -i "s#^key_buffer_size.*#key_buffer_size = 1024M#" /etc/my.cnf
+        sed -i "s#^table_open_cache.*#table_open_cache = 4096#" /etc/my.cnf
+        sed -i "s#^sort_buffer_size.*#sort_buffer_size = 16M#" /etc/my.cnf
+        sed -i "s#^read_buffer_size.*#read_buffer_size = 16M#" /etc/my.cnf
+        sed -i "s#^myisam_sort_buffer_size.*#myisam_sort_buffer_size = 256M#" /etc/my.cnf
+        sed -i "s#^thread_cache_size.*#thread_cache_size = 512#" /etc/my.cnf
+        sed -i "s#^query_cache_size.*#query_cache_size = 512M#" /etc/my.cnf
+        sed -i "s#^tmp_table_size.*#tmp_table_size = 512M#" /etc/my.cnf
+        sed -i "s#^innodb_buffer_pool_size.*#innodb_buffer_pool_size = 4096M#" /etc/my.cnf
+        sed -i "s#^innodb_log_file_size.*#innodb_log_file_size = 1024M#" /etc/my.cnf
+    fi
 }
 
 isCentos7=`cat /etc/redhat-release | grep 7\..* | grep -i centos`
@@ -568,6 +670,8 @@ if [ "${isCentos7}" != '' ];then
 else
   centos='6'
 fi
+
+MemTotal=`free -m | grep Mem | awk '{print  $2}'`
 
 echo '=======================================================';
 echo 'Your select to install:'
