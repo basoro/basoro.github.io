5f2c3bcec0ad56094ee49529198aa428da6f04a5
Basoro <dentix.id@gmail.com>
2019-07-03 06:12:46 +0800

    Update imagemagick.sh

--- downloads/3/imagemagick.sh
+++ downloads/3/imagemagick.sh
@@ -3,6 +3,9 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+. /www/server/panel/script/public.sh
+download_Url=$NODE_URL
+
 cpuInfo=$(getconf _NPROCESSORS_ONLN)
 if [ "${cpuInfo}" -ge "4" ];then
 	cpuCore=$((${cpuInfo}-1))
