3a0f4fe0e5799eb7f6acf6b6d806b3e8c5db1fb3
Basoro <dentix.id@gmail.com>
2019-07-03 06:13:12 +0800

    Update imap.sh

--- downloads/3/imap.sh
+++ downloads/3/imap.sh
@@ -2,6 +2,10 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
+
+. /www/server/panel/script/public.sh
+download_Url=$NODE_URL
+
 Install_Imap()
 {
 	isInstall=`cat /www/server/php/$version/etc/php.ini|grep 'imap.so'`
