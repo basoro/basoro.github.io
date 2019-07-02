3f1e7524ba10a539e3ff1bf160bda18773f3bffd
Basoro <dentix.id@gmail.com>
2019-07-03 06:15:07 +0800

    Update nginx.sh

--- downloads/3/nginx.sh
+++ downloads/3/nginx.sh
@@ -3,6 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+. /www/server/panel/script/public.sh
 download_Url=$NODE_URL
 
 tengine='2.3.0'
