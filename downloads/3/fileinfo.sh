d7b30ad88444e70b04651a4ec5644ccb810460f0
Basoro <dentix.id@gmail.com>
2019-07-03 06:12:23 +0800

    Update fileinfo.sh

--- downloads/3/fileinfo.sh
+++ downloads/3/fileinfo.sh
@@ -2,6 +2,9 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+. /www/server/panel/script/public.sh
+download_Url=$NODE_URL
+
 Install_Fileinfo()
 {
 	if [ ! -f "/www/server/php/$version/bin/php-config" ];then
