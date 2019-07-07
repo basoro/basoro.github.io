c7a62f77522317a5ab779514dd04ad88d988b242
Basoro <dentix.id@gmail.com>
2019-07-07 11:09:05 +0800

    update

--- downloads/slemp/lib.sh
+++ downloads/slemp/lib.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 . /opt/slemp/server/panel/script/public.sh
-download_Url=$NODE_URL
+download_Url=https://basoro.id/downloads/slemp
 
 mkdir -p /opt/slemp/server
 run_path="/root"
