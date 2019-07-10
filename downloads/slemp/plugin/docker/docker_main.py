fc6956497be192531b8f22dc18a4727bc28dd606
Basoro <dentix.id@gmail.com>
2019-07-10 23:06:46 +0800

    update

--- downloads/slemp/plugin/docker/docker_main.py
+++ downloads/slemp/plugin/docker/docker_main.py
@@ -7,7 +7,7 @@ if sys.version_info[0] == 2:
     sys.setdefaultencoding('utf-8')
 os.chdir('/opt/slemp/server/panel');
 sys.path.append("")
-import docker, public, json
+import public, json
 
 
 class docker_main(object):
