1a3aa05077650438090ef029a3a6ffd749b327db
Basoro <dentix.id@gmail.com>
2019-07-12 05:43:12 +0800

    update

--- downloads/slemp/plugin/docker/install.sh
+++ downloads/slemp/plugin/docker/install.sh
@@ -27,7 +27,10 @@ Install_Docker_ce()
 	#install docker-ce
 	if [ -f "/usr/bin/apt-get" ];then
 		apt-get remove docker docker-engine docker.io -y
-		apt install docker.io -y
+		apt install apt-transport-https ca-certificates curl software-properties-common -y
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+		add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+		apt-get install docker-ce -y
 	else
 		yum remove docker docker-common docker-selinux docker-engine -y
 		yum install -y yum-utils device-mapper-persistent-data lvm2 -y
