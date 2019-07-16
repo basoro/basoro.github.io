6422a52b8a5dbcde40ae67c1a52856e7c90ba615
Basoro <dentix.id@gmail.com>
2019-07-17 01:08:16 +0800

    update

---
+++ downloads/slemp/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone https://basoro:415basoro@github.com/basoro/slemp.git
+yes | cp -a slemp/*.py /opt/slemp/server/panel
+yes | cp -a slemp/static/* /opt/slemp/server/panel/static/
+yes | cp -a slemp/templates/* /opt/slemp/server/panel/templates/
+python -m compileall /opt/slemp/server/panel/
+rm -f /opt/slemp/server/panel/*.py
+rm -rf slemp
+slemp restart
