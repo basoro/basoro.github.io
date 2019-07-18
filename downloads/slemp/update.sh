2bdb2952061e64b6648ba0ff963e337c36abd8e6
Basoro <dentix.id@gmail.com>
2019-07-18 17:27:14 +0800

    update

--- downloads/slemp/update.sh
+++ downloads/slemp/update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-git clone https://github.com/basoro/slemp.git
-yes | cp -a slemp/*.py /opt/slemp/server/panel
-yes | cp -a slemp/static/* /opt/slemp/server/panel/static/
-yes | cp -a slemp/templates/* /opt/slemp/server/panel/templates/
+wget -O panel.zip https://basoro.id/downloads/slemp/panel.zip -T 10
+wget -O /etc/init.d/slemp https://basoro.id/downloads/slemp/init/slemp.init -T 10
+unzip panel.zip
+yes | cp -a panel/*.py /opt/slemp/server/panel
+yes | cp -a panel/static/* /opt/slemp/server/panel/static/
+yes | cp -a panel/templates/* /opt/slemp/server/panel/templates/
 python -m compileall /opt/slemp/server/panel/
 rm -f /opt/slemp/server/panel/*.py
-rm -rf slemp
-wget -O /etc/init.d/slemp $download_Url/init/slemp.init -T 10
+rm -f panel.zip
 chmod +x /etc/init.d/slemp
 slemp restart
