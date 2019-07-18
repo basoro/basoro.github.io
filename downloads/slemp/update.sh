2aff79bd3a44beab1f6b9a68cfe4283363b192c6
Basoro <dentix.id@gmail.com>
2019-07-18 17:24:49 +0800

    update

--- downloads/slemp/update.sh
+++ downloads/slemp/update.sh
@@ -6,4 +6,6 @@ yes | cp -a slemp/templates/* /opt/slemp/server/panel/templates/
 python -m compileall /opt/slemp/server/panel/
 rm -f /opt/slemp/server/panel/*.py
 rm -rf slemp
+wget -O /etc/init.d/slemp $download_Url/init/slemp.init -T 10
+chmod +x /etc/init.d/slemp
 slemp restart
