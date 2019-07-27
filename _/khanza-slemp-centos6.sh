3e99ef64953c13e4823d8ab9107ced8cd85eb151
Basoro <dentix.id@gmail.com>
2019-07-28 05:13:03 +0800

    Update

--- _/khanza-slemp-centos6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-
-if [ -f "/usr/bin/wget" ];then
-	wget -O install.sh https://raw.githubusercontent.com/basoro/basoro.github.io/master/_/slemp.sh && sh install.sh
-	exit;
-fi
--- _/slemp-khanza/GetCodeImage.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once './class/Code.class.php';
-@session_start();
-$codeImage = new Code(30);
-$codeImage->getImage();
-?>
