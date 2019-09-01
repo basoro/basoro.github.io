c5af7e21107a4fccee2b2173b35489bed77dc980
Basoro <dentix.id@gmail.com>
2019-09-01 20:06:53 +0800

    Update

---
+++ downloads/slemp/plugin/smsgateway/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+download_Url=https://basoro.id/downloads/slemp
+install_tmp='/tmp/slemp_install.pl'
+public_file=/opt/slemp/server/panel/script/public.sh
+if [ ! -f $public_file ];then
+	wget -O $public_file $download_Url/public.sh -T 5;
+fi
+. $public_file
+
+pluginPath=/opt/slemp/server/panel/plugin/smsgateway
+
+Install_Smsgateway()
+{
+	mkdir -p $pluginPath
+	echo 'Installing script file...' > $install_tmp
+	wget -O $pluginPath/smsgateway_main.py $download_Url/plugin/smsgateway/smsgateway_main.py -T 5
+	wget -O $pluginPath/index.html $download_Url/plugin/smsgateway/index.html -T 5
+	wget -O $pluginPath/info.json $download_Url/plugin/smsgateway/info.json -T 5
+	echo 'The installation is complete' > $install_tmp
+
+
+}
+
+Uninstall_Smsgateway()
+{
+	rm -rf $pluginPath
+}
+
+
+action=$1
+if [ "${1}" == 'install' ];then
+	Install_Smsgateway
+else
+	Uninstall_Smsgateway
+fi
