da811dbf9d861340df3959f53dc8606c6354abfd
Basoro <dentix.id@gmail.com>
2021-11-18 15:16:57 +0800

    Update install.sh

--- downloads/slemp/plugin/gdrive/install.sh
+++ downloads/slemp/plugin/gdrive/install.sh
@@ -13,7 +13,10 @@ pluginPath=/opt/slemp/server/panel/plugin/gdrive
 
 Install_GDrive()
 {
-pip install --upgrade google-auth==1.6.3 google-api-python-client==1.7.11 google-auth-httplib2==0.0.3 google-auth-oauthlib==0.4.0 cachetools==3.1.1
+#pip install --upgrade google-auth==1.6.3 google-api-python-client==1.7.11 google-auth-httplib2==0.0.3 google-auth-oauthlib==0.4.0 cachetools==3.1.1
+pip install -I pyOpenSSL
+pip install -I google-api-python-client==1.7.11 google-auth-httplib2==0.0.3 google-auth-oauthlib==0.4.1 -i https://pypi.Python.org/simple
+pip install -I httplib2==0.15.0 -i https://pypi.Python.org/simple
 mkdir -p $pluginPath
 echo 'Installing script file...' > $install_tmp
 	wget -O /opt/slemp/server/panel/script/backup_gdrive.py $download_Url/plugin/gdrive/gdrive_main.py -T 5
