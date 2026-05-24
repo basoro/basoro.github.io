62a1311cd852455228811db87c635316af612fd6
Basoro <dentix.id@gmail.com>
2026-05-25 05:34:27 +0800

    Enhance slemp4_dev.sh with OS detection and logging
    Updated the script to include additional OS checks and installation commands for various distributions, improved logging, and added color-coded output.

--- slemp4_dev.sh
+++ slemp4_dev.sh
@@ -1,79 +1,174 @@
 #!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/opt/homebrew/bin
 export PATH
-# LANG=en_US.UTF-8
-is64bit=`getconf LONG_BIT`
+
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+PLAIN='\033[0m'
+BOLD='\033[1m'
+SUCCESS='[\033[32mOK\033[0m]'
+COMPLETE='[\033[32mDONE\033[0m]'
+WARN='[\033[33mWARN\033[0m]'
+ERROR='[\033[31mERROR\033[0m]'
+WORKING='[\033[34m*\033[0m]'
+
+# Path detection
+OSNAME=$(uname -s)
+if [ "$OSNAME" == "Darwin" ]; then
+    DIR=$(cd "$(dirname "$0")"; pwd)
+    export rootPath=$(dirname "$DIR")
+    export serverPath=$(dirname "$rootPath")
+else
+    export rootPath="/opt/slemp/server/panel"
+    export serverPath="/opt/slemp/server"
+fi
+
+LOG_FILE=slemp-install.log
+{
+
+HTTP_PREFIX="https://"
 
 if [ -f /etc/motd ];then
-    echo "welcome to SLEMP panel" > /etc/motd
+    echo "welcome to panel panel" > /etc/motd
 fi
 
 startTime=`date +%s`
 
 _os=`uname`
-echo "use system: ${_os}"
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root!"
-  exit
-fi
-
+echo "menggunakan sistem: ${_os}"
 
 if [ ${_os} == "Darwin" ]; then
 	OSNAME='macos'
-elif grep -Eq "openSUSE" /etc/*-release; then
+elif grep -Eqi "openSUSE" /etc/*-release; then
 	OSNAME='opensuse'
 	zypper refresh
-elif grep -Eq "FreeBSD" /etc/*-release; then
+	zypper install cron wget curl zip unzip
+elif grep -Eqi "FreeBSD" /etc/*-release; then
 	OSNAME='freebsd'
-elif grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
-	OSNAME='centos'
-	yum install -y wget zip unzip
-elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
-	OSNAME='fedora'
-	yum install -y wget zip unzip
-elif grep -Eqi "Rocky" /etc/issue || grep -Eq "Rocky" /etc/*-release; then
-	OSNAME='rocky'
-	yum install -y wget zip unzip
-elif grep -Eqi "AlmaLinux" /etc/issue || grep -Eq "AlmaLinux" /etc/*-release; then
-	OSNAME='alma'
-	yum install -y wget zip unzip
-elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+	pkg install -y wget curl zip unzip unrar rar
+elif grep -Eqi "EulerOS" /etc/*-release || grep -Eqi "openEuler" /etc/*-release; then
+	OSNAME='euler'
+	yum install -y wget curl zip unzip tar crontabs
+elif grep -Eqi "CentOS" /etc/issue || grep -Eqi "CentOS" /etc/*-release; then
+	OSNAME='rhel'
+	yum install -y wget curl zip unzip tar crontabs
+elif grep -Eqi "Fedora" /etc/issue || grep -Eqi "Fedora" /etc/*-release; then
+	OSNAME='rhel'
+	yum install -y wget curl zip unzip tar crontabs
+elif grep -Eqi "Rocky" /etc/issue || grep -Eqi "Rocky" /etc/*-release; then
+	OSNAME='rhel'
+	yum install -y wget curl zip unzip tar crontabs
+elif grep -Eqi "Anolis" /etc/issue || grep -Eqi "Anolis" /etc/*-release; then
+	OSNAME='rhel'
+	yum install -y wget curl zip unzip tar crontabs
+elif grep -Eqi "AlmaLinux" /etc/issue || grep -Eqi "AlmaLinux" /etc/*-release; then
+	OSNAME='rhel'
+	yum install -y wget curl zip unzip tar crontabs
+elif grep -Eqi "Amazon Linux" /etc/issue || grep -Eqi "Amazon Linux" /etc/*-release; then
+	OSNAME='amazon'
+	yum install -y wget curl zip unzip tar crontabs
+elif grep -Eqi "Ubuntu" /etc/issue || grep -Eqi "Ubuntu" /etc/os-release; then
+	OSNAME='ubuntu'
+	apt update -y
+	apt install -y wget curl zip unzip tar cron
+elif grep -Eqi "Debian" /etc/issue || grep -Eqi "Debian" /etc/os-release; then
 	OSNAME='debian'
 	apt update -y
-	apt install -y devscripts
-	apt install -y wget zip unzip
-elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
-	OSNAME='ubuntu'
-	apt install -y wget zip unzip
+	apt install -y wget curl zip unzip tar cron
+elif grep -Eqi "Alpine" /etc/issue || grep -Eqi "Alpine" /etc/*-release; then
+	OSNAME='alpine'
+	apk update
+	apk add devscripts -force-broken-world
+	apk add wget zip unzip tar -force-broken-world
 else
 	OSNAME='unknow'
 fi
 
+if [ "$EUID" -ne 0 ] && [ "$OSNAME" != "macos" ];then 
+	echo "Harap jalankan sebagai root!"
+ 	exit
+fi
+
+echo "OSNAME:${OSNAME}"
 
 if [ $OSNAME != "macos" ];then
-	mkdir -p /opt/slemp/server
-	mkdir -p /opt/slemp/wwwroot
-	mkdir -p /opt/slemp/wwwlogs
-	mkdir -p /opt/slemp/backup/database
-	mkdir -p /opt/slemp/backup/site
+	if id www &> /dev/null ;then 
+	    echo ""
+	else
+	    groupadd www
+		useradd -g www -s /usr/sbin/nologin www
+	fi
 
-	if [ ! -d /opt/slemp/server/panel ];then
-		wget -O /tmp/master.zip https://codeload.github.com/basoro/slemp/zip/master
-		cd /tmp && unzip /tmp/master.zip
-		mv -f /tmp/slemp-master /opt/slemp/server/panel
-		rm -rf /tmp/master.zip
+	mkdir -p ${serverPath}
+	mkdir -p $(dirname "$serverPath")/wwwroot
+	mkdir -p $(dirname "$serverPath")/wwwlogs
+	mkdir -p $(dirname "$serverPath")/backup/database
+	mkdir -p $(dirname "$serverPath")/backup/site
+
+	if [ ! -d ${rootPath} ];then
+		curl --insecure -sSLo /tmp/master.tar.gz ${HTTP_PREFIX}github.com/basoro/slemp/archive/refs/heads/master.tar.gz
+		cd /tmp && tar -zxvf /tmp/master.tar.gz
+		mv -f /tmp/slemp-master ${rootPath}
+		rm -rf /tmp/master.tar.gz
 		rm -rf /tmp/slemp-master
+	fi
+
+	# install acme.sh
+	if [ ! -d /root/.acme.sh ];then
+	    if [ "$LOCAL_ADDR" != "common" ];then
+	        curl --insecure -sSLo /tmp/acme.sh-master.tar.gz ${HTTP_PREFIX}github.com/acmesh-official/acme.sh/archive/refs/heads/master.tar.gz
+	        tar xvzf /tmp/acme.sh-master.tar.gz -C /tmp
+	        cd /tmp/acme.sh-master
+	        bash acme.sh install
+	    else
+	    	curl -fsSL https://get.acme.sh | bash
+	    fi
 	fi
 fi
 
-echo "use system version: ${OSNAME}"
-cd /opt/slemp/server/panel && bash scripts/install/${OSNAME}.sh
+echo "menggunakan versi sistem: ${OSNAME}"
+if [ "${OSNAME}" == "macos" ];then
+	bash scripts/install/macos.sh
+else
+	cd ${rootPath} && bash scripts/install/${OSNAME}.sh
+fi
+
+if [ "${OSNAME}" == "macos" ];then
+	echo "macos selesai"
+	exit 0
+fi
+
+cd ${rootPath} && bash cli.sh start
+isStart=`ps -ef|grep 'gunicorn -c setting.py app:app' |grep -v grep|awk '{print $2}'`
+n=0
+while [ ! -f /etc/rc.d/init.d/slemp ];
+do
+    echo -e ".\c"
+    sleep 1
+    let n+=1
+    if [ $n -gt 20 ];then
+    	echo -e "mulai mw gagal"
+    	exit 1
+    fi
+done
+
+cd ${rootPath} && bash /etc/rc.d/init.d/slemp stop
+cd ${rootPath} && bash /etc/rc.d/init.d/slemp start
+cd ${rootPath} && bash /etc/rc.d/init.d/slemp default
+
+sleep 2
+if [ ! -e /usr/bin/slemp ]; then
+	if [ -f /etc/rc.d/init.d/slemp ];then
+		ln -s /etc/rc.d/init.d/slemp /usr/bin/slemp
+	fi
+fi
 
 endTime=`date +%s`
 ((outTime=(${endTime}-${startTime})/60))
-echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
+echo -e "Waktu yang dihabiskan:\033[32m $outTime \033[0mMenit!"
 
-if [ $OSNAME != "macos" ];then
-	systemctl daemon-reload
-fi
+} 1> >(tee $LOG_FILE) 2>&1
+
+echo -e "\nInstalasi selesai. Jika terjadi kesalahan, silakan periksa file log slemp-install.log ."
